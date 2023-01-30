e71dee4f745569458c798c53762a9554a23dc93c
shreyas-sharma <shreyasshourya9@gmail.com>
2023-01-30 15:10:25 -0600

    Initial commit

---
+++ react-login-signup-ui-template/src/components/signup.component.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+
+
+export default class SignUp extends Component {
+  render() {
+    return (
+      <form>
+        <h3>Sign Up</h3>
+        <div className="mb-3">
+          <label>First name</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="First name"
+          />
+        </div>
+        <div className="mb-3">
+          <label>Last name</label>
+          <input type="text" className="form-control" placeholder="Last name" />
+        </div>
+        <div className="mb-3">
+          <label>Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+          />
+        </div>
+        <div className="mb-3">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+          />
+        </div>
+        <div className="d-grid">
+          <Link to="/payment">
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </Link>
+        </div>
+        <p className="forgot-password text-right">
+          Already registered <a href="/sign-in">sign in?</a>
+        </p>
+      </form>
+    )
+  }
+}
